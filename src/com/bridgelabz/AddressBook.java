8b0a36dba28eec57091796d6d0c96ce76d5e0072
Rohan <rohandute1@gmail.com>
2023-11-03 13:52:37 +0530

    Added:- successfully added functionality to delete contact from address book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,10 +1,13 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
     Scanner sc = new Scanner(System.in);
     Contact person = new Contact();
+    List<Contact> contactList = new ArrayList<>();
     public void addContact(){
         System.out.println("Enter first name :");
         String firstName = sc.next();
@@ -38,49 +41,64 @@ public class AddressBook {
         String email = sc.next();
         person.setEmail(email);
 
-        System.out.println(person);
+        contactList.add(person);
+
+        System.out.println(contactList);
     }
     public void editContact(){
         System.out.println("Please enter the Name of which you want to edit :");
         String name = sc.next();
-        if(name.equals(person.getFirstName())){
-            System.out.println("Select option from below:");
-            System.out.println("1.Change first name.\n2.Change last name\n3.Change address\n4.Change city\n5.Change state" +
-                    "\n6.Change Phone number\n7.Change email");
-            int condition = sc.nextInt();
-            switch(condition){
-                case 1:
-                    person.setFirstName(sc.next());
-                    break;
-                case 2:
-                    person.setLastName(sc.next());
-                    break;
-                case 3:
-                    person.setAddress(sc.next());
-                    break;
-                case 4:
-                    person.setCity(sc.next());
-                    break;
-                case 5:
-                    person.setState(sc.next());
-                    break;
-                case 6:
-                    person.setZip(sc.nextInt());
-                    break;
-                case 7:
-                    person.setPhone(sc.nextInt());
-                    break;
-                case 8:
-                    person.setEmail(sc.next());
-                    break;
+        for (int i = 0;i<contactList.size();i++) {
+            if (name.equals(person.getFirstName())) {
+                System.out.println("Select option from below:");
+                System.out.println("1.Change first name.\n2.Change last name\n3.Change address\n4.Change city\n5.Change state" +
+                        "\n6.Change zip code\n7.Change Phone number\n8.Change email");
+                int condition = sc.nextInt();
+                switch (condition) {
+                    case 1:
+                        contactList.get(i).setFirstName(sc.next());
+                        break;
+                    case 2:
+                        contactList.get(i).setLastName(sc.next());
+                        break;
+                    case 3:
+                        contactList.get(i).setAddress(sc.next());
+                        break;
+                    case 4:
+                        contactList.get(i).setCity(sc.next());
+                        break;
+                    case 5:
+                        contactList.get(i).setState(sc.next());
+                        break;
+                    case 6:
+                        contactList.get(i).setZip(sc.nextInt());
+                        break;
+                    case 7:
+                        contactList.get(i).setPhone(sc.nextInt());
+                        break;
+                    case 8:
+                        contactList.get(i).setEmail(sc.next());
+                        break;
+                }
             }
         }
-        System.out.println(person);
+        System.out.println(contactList);
+    }
+    public void deleteContact(){
+        System.out.println("Please enter the name of which you wants to delete the contact :");
+        String name = sc.next();
+        for(int i = 0; i<contactList.size();i++) {
+            if (name.equals(contactList.get(i).getFirstName())) {
+                contactList.remove(i);
+            }
+        }
+        System.out.println(contactList);
     }
     public static void main(String[] args){
         System.out.println("Welcome to address book management system.");
         AddressBook addressBook = new AddressBook();
         addressBook.addContact();
         addressBook.editContact();
+        addressBook.deleteContact();
     }
 }
