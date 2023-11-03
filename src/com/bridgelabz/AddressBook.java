af4668fed71265b9aa5fd7c7ff3b9cb1b8d4dd3b
Rohan <rohandute1@gmail.com>
2023-11-03 12:39:16 +0530

    Added:- successfully added functionality to edit contact in the address book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -40,9 +40,47 @@ public class AddressBook {
 
         System.out.println(person);
     }
+    public void editContact(){
+        System.out.println("Please enter the Name of which you want to edit :");
+        String name = sc.next();
+        if(name.equals(person.getFirstName())){
+            System.out.println("Select option from below:");
+            System.out.println("1.Change first name.\n2.Change last name\n3.Change address\n4.Change city\n5.Change state" +
+                    "\n6.Change Phone number\n7.Change email");
+            int condition = sc.nextInt();
+            switch(condition){
+                case 1:
+                    person.setFirstName(sc.next());
+                    break;
+                case 2:
+                    person.setLastName(sc.next());
+                    break;
+                case 3:
+                    person.setAddress(sc.next());
+                    break;
+                case 4:
+                    person.setCity(sc.next());
+                    break;
+                case 5:
+                    person.setState(sc.next());
+                    break;
+                case 6:
+                    person.setZip(sc.nextInt());
+                    break;
+                case 7:
+                    person.setPhone(sc.nextInt());
+                    break;
+                case 8:
+                    person.setEmail(sc.next());
+                    break;
+            }
+        }
+        System.out.println(person);
+    }
     public static void main(String[] args){
         System.out.println("Welcome to address book management system.");
         AddressBook addressBook = new AddressBook();
         addressBook.addContact();
+        addressBook.editContact();
     }
 }
